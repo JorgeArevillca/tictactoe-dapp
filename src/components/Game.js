82c019e1215c73f0b337b02630c3fc6934cfde8b
David Sato <david.sato64@gmail.com>
2018-01-05 14:46:31 +0100

    moved contract activation from Game to index.js as callback[async]

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,34 +1,10 @@
 import React, { Component } from 'react'
 
-import TruffleContract from 'truffle-contract'
-import Web3 from 'web3'
-
-import TicTacToeContractData from '../../build/contracts/TicTacToe.json'
-
 import Block from 'layouts/Block'
 
-var web3 = new Web3('http://localhost:8545');
-
-class Game extends Component {
-  async componentWillMount() {
-    const contract = TruffleContract(TicTacToeContractData)
-    console.log(contract)
-    console.log(TicTacToeContractData)
-    const provider = (await web3).currentProvider
-
-    console.log(provider)
-    await contract.setProvider(provider)
-    const deployedContract = await contract.deployed()
-    console.log(deployedContract)
-  }
-
-  render() {
-    return (
-      <Block>
-  hi
-      </Block>
-    )
-  }
-}
+const Game = () =>
+  <Block>
+    hi
+  </Block>
 
 export default Game
--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 import React from 'react'
 import { render } from 'react-dom'
 
+import TruffleContract from 'truffle-contract'
+import Web3 from 'web3'
+
+import TicTacToeContractData from '../build/contracts/TicTacToe.json'
+
 import Page from 'components/Page'
 
 import 'assets/scss/style.scss'
 
-render(<Page />, document.getElementById("root"))
+const setUpContracts = async () => {
+  try {
+    const contract = TruffleContract(TicTacToeContractData)
+    const { currentProvider } = await web3
+
+    const csp = await contract.setProvider(currentProvider)
+    console.log(csp)
+    const deployedContract = await contract.deployed()
+    console.log(deployedContract)
+  } catch (e) {
+    throw new Error (e)
+  }
+}
+
+render(<Page />, document.getElementById("root"), setUpContracts)
