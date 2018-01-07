e07c33633f12e26ab2cbc96056e88d070b3c921b
David Sato <david.sato64@gmail.com>
2018-01-07 18:21:17 +0100

    added TTTF functionality and event functionality

--- contracts/TicTacToeFactory.sol
+++ contracts/TicTacToeFactory.sol
@@ -4,8 +4,14 @@ import './TicTacToe.sol';
 
 contract TicTacToeFactory {
 
+  event BroadCastTTTAddress(address TTTGame);
+
   function newGame() public returns (address) {
     return new TicTacToe(msg.sender);
+  }
+
+  function broadcastNewTTTAddress() public {
+    BroadCastTTTAddress(msg.sender);
   }
 
 }
--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import { render } from 'react-dom'
 
 import Page from 'components/Page'
 
-// import { setUpContracts } from 'api'
-
 import 'assets/scss/style.scss'
 
 render(<Page />, document.getElementById("root"))
