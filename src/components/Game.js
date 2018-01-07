8ac3aa127fb9754c14db9db53d6b8f07a191f744
Andre Meyer <andre.meyer93@gmail.com>
2018-01-07 19:22:38 +0100

    add manual join

--- src/components/Game.js
+++ src/components/Game.js
@@ -22,6 +22,8 @@ class Game extends Component {
   constructor(props) {
     super(props)
     this.handleCreateGame = this.handleCreateGame.bind(this)
+    this.handleManuallyJoinGame = this.handleManuallyJoinGame.bind(this)
+    this.handleChangeJoinAddress = this.handleChangeJoinAddress.bind(this)
     this.state = {
       gameFactoryReady: false,
       gameAddress: undefined,
@@ -35,6 +37,28 @@ class Game extends Component {
     
     this.setState({
       gameFactoryReady: true
+    })
+  }
+
+  async handleManuallyJoinGame() {
+    const { joinAddress } = this.state
+
+    try {
+      this.contractClass = await getContract('TicTacToe')
+      this.contract = await this.contractClass.at(joinAddress)
+
+      this.setState({
+        gameAddress: joinAddress
+      })
+    } catch (e) {
+      console.log("failed to join game", e)
+      alert("invalid game, could not join")
+    }
+  }
+
+  async handleChangeJoinAddress(e) {
+    this.setState({
+      joinAddress: e.target.value
     })
   }
 
@@ -79,7 +103,10 @@ class Game extends Component {
             })}
           </div>
         ) : (
-          <span>No games available... {this.state.gameFactoryReady && <button type="button" onClick={this.handleCreateGame}>Create a new Game</button>}</span>
+          <div>
+            <p>No games available... {this.state.gameFactoryReady && <button type="button" onClick={this.handleCreateGame}>Create a new Game</button>}</p>
+            <p>Or manually join game (by address): <input type="text" value={this.state.joinAddress || ''} onChange={this.handleChangeJoinAddress} /><button type="button" onClick={this.handleManuallyJoinGame}>Join Game</button></p>
+          </div>
         )}
       </div>
     )
@@ -91,21 +118,24 @@ class Game extends Component {
     }
 
     return (
-      <div className={styles.game}>
-        <div className={styles.gameFieldRow}>
-          <Field state={STATES.CIRCLE} />
-          <Field state={STATES.CIRCLE} />
-          <Field state={STATES.CIRCLE} />
-        </div>
-        <div className={styles.gameFieldRow}>
-          <Field state={STATES.EMTPY} />
-          <Field state={STATES.CROSS} />
-          <Field state={STATES.EMTPY} />
-        </div>
-        <div className={styles.gameFieldRow}>
-          <Field state={STATES.EMTPY} />
-          <Field state={STATES.EMTPY} />
-          <Field state={STATES.EMTPY} />
+      <div>
+        <p>Your game's address is <code>{this.state.gameAddress}</code></p>
+        <div className={styles.game}>
+          <div className={styles.gameFieldRow}>
+            <Field state={STATES.CIRCLE} />
+            <Field state={STATES.CIRCLE} />
+            <Field state={STATES.CIRCLE} />
+          </div>
+          <div className={styles.gameFieldRow}>
+            <Field state={STATES.EMTPY} />
+            <Field state={STATES.CROSS} />
+            <Field state={STATES.EMTPY} />
+          </div>
+          <div className={styles.gameFieldRow}>
+            <Field state={STATES.EMTPY} />
+            <Field state={STATES.EMTPY} />
+            <Field state={STATES.EMTPY} />
+          </div>
         </div>
       </div>
     )
