e0016a4d88241838735c7f0cff734f6d6dfeb094
David Sato <david.sato64@gmail.com>
2018-01-09 17:34:11 +0100

    event cb working

--- src/index.js
+++ src/index.js
@@ -10,13 +10,18 @@ import Game from 'components/Game'
 import { Router, Route, Switch } from 'react-router'
 import { HashRouter } from 'react-router-dom'
 
+import Web3 from 'web3'
 import TC from 'truffle-contract'
+
+Web3.providers.HttpProvider.prototype.sendAsync = Web3.providers.HttpProvider.prototype.send
 const TTTF = TC(require('../build/contracts/TicTacToeFactory.json'))
+const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'))
 
 const callbackEvent = async () => {
-  console.log('Andre is lame!')
+  console.log('Andre is nicht sehr lame! Aber er ist viel viel geile!')
+  
+  await TTTF.setProvider(web3.currentProvider)
   const { BroadCastTTTAddress: bcAdd } = await TTTF.deployed()
-
   bcAdd().watch((err, resp) => {
     if (err) throw new Error(err)
     console.log(resp)
