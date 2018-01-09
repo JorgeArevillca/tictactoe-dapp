e660ead574f19015ada9bbd836b1b5c0c3c95073
David Sato <david.sato64@gmail.com>
2018-01-09 17:24:07 +0100

    testing events

--- src/index.js
+++ src/index.js
@@ -10,6 +10,19 @@ import Game from 'components/Game'
 import { Router, Route, Switch } from 'react-router'
 import { HashRouter } from 'react-router-dom'
 
+import TC from 'truffle-contract'
+const TTTF = TC(require('../build/contracts/TicTacToeFactory.json'))
+
+const callbackEvent = async () => {
+  console.log('Andre is lame!')
+  const { BroadCastTTTAddress: bcAdd } = await TTTF.deployed()
+
+  bcAdd().watch((err, resp) => {
+    if (err) throw new Error(err)
+    console.log(resp)
+  })
+}
+
 render((
   <HashRouter>
     <Page>
@@ -17,4 +30,4 @@ render((
       <Route path={'/:gameAddress'} component={Game} />
     </Page>
   </HashRouter>
-), document.getElementById("root"))
+), document.getElementById("root"), callbackEvent)
